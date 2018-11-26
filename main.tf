711de05f6ee323537d199ed4d414478008d6b6c4
Alex <netboi@pop-os.localdomain>
2018-11-26 14:02:26 -0500

    adding notes for team members

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-Example Usage
+# Example Usage
 # Configure the Packet Provider
 provider "packet" {
-  auth_token = "${var.auth_token}"
+  auth_token = "${var.auth_token}" #need to change this when account approved
 }
 
 # Create a project
@@ -12,9 +12,9 @@ resource "packet_project" "portsandbox-proj" {
 
 # Create a device and add it to tf_project_1
 resource "packet_device" "web1" {
-  hostname         = "tf.coreos2"
-  plan             = "baremetal_1"
-  facility         = "ewr1"
+  hostname         = "c2.medium.x8" #change this to support AMD EPYC 
+  plan             = "baremetal_1"  #spot instances on-demand would be cheaper
+  facility         = "ewr1"	    #have this tie into the UI to change depending on location of school
   operating_system = "coreos_stable"
   billing_cycle    = "hourly"
   project_id       = "${packet_project.cool_project.id}"
