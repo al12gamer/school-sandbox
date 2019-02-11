f64dfb63a46fa2ab01fd4a5d407987e5c6a8679e
Alex Seibz <al12gamer@gmail.com>
2019-02-11 12:29:38 -0500

    new additions to terrform

--- main.tf
+++ main.tf
@@ -1,21 +1,20 @@
-# Example Usage
 # Configure the Packet Provider
 provider "packet" {
-  auth_token = "${var.auth_token}" #need to change this when account approved
+  auth_token = "${var.auth_token}"
 }
 
 # Create a project
-resource "packet_project" "portsandbox-proj" {
-  name           = "Portable Sandbox Project"
-  payment_method = "PAYMENT_METHOD_ID"          # use Alex Personal while still testing
+resource "packet_project" "" {
+  name           = "Sandbox Example"
+  payment_method = "PAYMENT_METHOD_ID"          # Only required for a non-default payment method
 }
 
-# Create a device and add it to tf_project_1
-resource "packet_device" "web1" {
-  hostname         = "c2.medium.x8" #change this to support AMD EPYC 
-  plan             = "baremetal_1"  #spot instances on-demand would be cheaper
-  facility         = "ewr1"	    #have this tie into the UI to change depending on location of school
-  operating_system = "coreos_stable"
+# Create a device and add it to sandbox1
+resource "packet_device" "sandbox1" {
+  hostname         = "tf.centos1"
+  plan             = "c1.small.x86"
+  facility         = "ewr1"
+  operating_system = "centos7"
   billing_cycle    = "hourly"
   project_id       = "${packet_project.cool_project.id}"
 }
