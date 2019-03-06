e81d78bb44d58455785ab34c2157d7aaa3825d34
al12gamer <al12gamer@gmail.com>
2019-03-06 00:29:03 -0500

    changing files

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
-# Configure the Packet Provider
 provider "packet" {
   auth_token = "${var.packet_api_key}"
 }
 
-# Create a project
-resource "packet_project" "" {
-  name = "Sandbox Example"
+resource "packet_project" "kvm-lab" {
+  name           = "Compute Hosts"
 }
 
-# Create a device and add it to sandbox1
-# This is just a test, still need to have proper tvars before setting up
-resource "packet_device" "sandbox1" {
-  hostname         = "tf.centos1"
-  plan             = "c1.small.x86"
-  facility         = "ewr1"
-  operating_system = "centos7"
+# this was copied from https://medium.com/@jmarhee/creating-a-kvm-virtualization-environment-on-packet-net-with-terraform-b29ed69fe6a5 for instructional purposes
+# this is being modified to spin up an environment
+resource "packet_device" "kvm-node" {
+  hostname = "${format("compute-%02d.kvm", count.index)}"
+  count = "${var.count}"
+  operating_system = "ubuntu_16_04"
   billing_cycle    = "hourly"
-  project_id       = "${packet_project.cool_project.id}"
-}
+  project_id       = "${packet_project.kvm-lab.id}"
+  plan             = "baremetal_0"
+  facility      = "${var.packet_facility}"
